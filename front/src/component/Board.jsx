9f532ecc384ca2b6821cea953c060235a462f396
Marcus Ortense <marcus.ortense@outlook.com>
2022-10-21 21:30:46 -0300

    prefere using hooks to abstract logic on components

--- front/src/component/Board.jsx
+++ front/src/component/Board.jsx
@@ -1,38 +1,22 @@
-import { useReducer } from 'react';
-import { fetchData } from '../repository/repository';
-import {ACTIONS,  IDLE_STATE, fetchDataReducer } from '../reducer/fetchData';
+import { useGameData } from '../hooks/useGamaData';
 import Game from './game/Game';
 import PlayerData from './statistics/PlayerData';
 import ResourceOdds from './statistics/ResourceOdds'
-
 import './Board.css'
 
-const Board = () => {
-  const [state, dispatch] = useReducer(fetchDataReducer, IDLE_STATE);
-  
-  const handleFetch = () => {
-    dispatch({ type: ACTIONS.fetching });
-    fetchData()
-      .then((data) => {
-        dispatch({ type: ACTIONS.success, payload: data });
-      })
-      .catch((error) => {
-        dispatch({ type: ACTIONS.error, error });
-      });
-    };
+export default function Board() {
+  const [{ ready, loading, data }, load] = useGameData()
   
   return (
-       <div key='board' id='board' className='board'>
-        <div key='game-board' id='game-board' className='gameBoard'>
-          { state.ready && < ResourceOdds statistics={state.data.statistics} /> }
-          { state.ready && <Game tiles={state.data.board.tiles} vertices={state.data.board.vertices} /> }
-        </div>
-        <div key='player-board' id='player-board' className='playerBoard'>
-          <button key='button' className='button' disabled={state.loading} onClick={handleFetch}>Fetch game data</button>
-          { state.ready && <PlayerData players={state.data.playerData}/>}
-        </div>
+    <div key='board' id='board' className='board'>
+      <div key='game-board' id='game-board' className='gameBoard'>
+        { ready && <ResourceOdds statistics={data.statistics} /> }
+        { ready && <Game tiles={data.board.tiles} vertices={data.board.vertices} /> }
       </div>
+      <div key='player-board' id='player-board' className='playerBoard'>
+        <button key='button' className='button' disabled={loading} onClick={load}>Fetch game data</button>
+        { ready && <PlayerData players={data.playerData}/>}
+      </div>
+    </div>
   )
 }
-
-export default Board;
---
+++ front/src/hooks/useGamaData.js
@@ -0,0 +1,4 @@
+import { fetchData } from '../repository/repository'
+import { useAsyncState } from './useAsyncState'
+
+export const useGameData = () => useAsyncState(fetchData)
